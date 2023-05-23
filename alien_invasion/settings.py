a606c305f1888b369668e604c6a9953149ea7c02
Mikhail <Misha9404@yandex.ru>
2023-05-23 15:57:48 +0300

    Start Project: - create file alien_invasion.py (bg color, add ship on screen) - create file settings.py (settings) - create file ship.py (add picture ship, add class Ship) - create directory Images and add picture ship.bmp

---
+++ alien_invasion/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана.
+        self.screen_width = 1900
+        self.screen_height = 950
+        self.bg_color = (230, 230, 230)
