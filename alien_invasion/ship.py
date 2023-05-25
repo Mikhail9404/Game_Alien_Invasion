60514c4905b7485cf07ac4c71a0dac4a4694500a
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-05-25 17:23:20 +0300

    add move ship

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -10,8 +10,21 @@ class Ship():
         # Загружает изображение корабля и получает прямоугольник.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Флаг перемещения.
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флага."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
