ef4176f756411940aa674c529a8efbbaab810d01
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-05-28 20:01:57 +0300

    Add: - controll speed ship; - move closure.

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -6,3 +6,6 @@ class Settings():
         self.screen_width = 1900
         self.screen_height = 950
         self.bg_color = (230, 230, 230)
+
+        # Настройка скорости корабля.
+        self.ship_speed = 1.5
