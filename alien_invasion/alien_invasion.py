1946c8bff59d39109f3f5d2586177703e431c63a
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-05-28 20:16:30 +0300

    Add: - exit key "Q"; - fullscreen window starting.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -12,9 +12,13 @@ class AlienInvasion:
         """Инициализация игры и создание игровых процессов."""
         pygame.init()
         self.settings = Settings()
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+#        self.screen = pygame.display.set_mode(
+#            (self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -47,6 +51,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
 
     def _check_keyup_events(self, event):
