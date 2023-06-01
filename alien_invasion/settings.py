0e6f36036d246668d75accfefbb842cf72e2038a
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-06-01 19:20:13 +0300

    Add: - shooting settings; - add class Bullet.

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,5 +7,11 @@ class Settings():
         self.screen_height = 950
         self.bg_color = (230, 230, 230)
 
+        # Параметры снаряда.
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
         # Настройка скорости корабля.
         self.ship_speed = 1.5
