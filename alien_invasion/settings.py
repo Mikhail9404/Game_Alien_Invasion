ef462d230a1bd3214caa86b9c1e8bd19c774fadd
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-06-03 19:00:13 +0300

    Allow count bullets and add method _update_bullets().

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -8,10 +8,11 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # Параметры снаряда.
-        self.bullet_speed = 2
+        self.bullet_speed = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 6
 
         # Настройка скорости корабля.
         self.ship_speed = 1.5
