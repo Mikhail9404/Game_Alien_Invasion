6a6c7b7f5589590026181dc33409f5c343a5ef6f
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-06-10 15:34:25 +0300

    Refactoring _create_fleet()

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -103,11 +103,16 @@ class AlienInvasion:
 
         # Создание первого ряда пришельцев.
         for alien_number in range(number_alien_x):
-            # Создание пришельца и размещение его в ряду.
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+
+
+    def _create_alien(self, alien_number):
+        # Создание пришельца и размещение его в ряду.
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
 
 
     def _update_screen(self):
