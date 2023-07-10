d9a8e2d52ceb978642a7571c2fceac5c6727a930
Mikhail <Misha9404@yandex.ru>
2023-07-10 16:20:52 +0300

    Upscale and downscale game level.

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,24 +1,41 @@
 class Settings():
 
     def __init__(self):
-        """Инициализирует настройки игры."""
+        """Инициализирует статичиские настройки игры."""
         # Параметры экрана.
         self.screen_width = 1900
         self.screen_height = 950
         self.bg_color = (230, 230, 230)
 
         # Параметры снаряда.
-        self.bullet_speed = 1.5
         self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullet_allowed = 6
+        self.bullet_allowed = 10
 
         # Настройка скорости корабля.
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Настройка пришельцев
-        self.alien_speed = 0.4
         self.fleet_drop_speed = 8
-        self.fleet_direction = 1 # Положительно значение - движение вправо, отрицательное - влево.
+
+        # Темп ускорения игры.
+        self.speedup_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходе игры."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 1.5
+        self.alien_speed = 0.4
+
+        self.fleet_direction = 1  # Положительно значение - движение вправо, отрицательное - влево.
+
+    def increase_speed(self):
+        """Увеличивает настройки скорости."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
