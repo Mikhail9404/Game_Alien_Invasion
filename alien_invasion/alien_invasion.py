f77962c90f4d33eca8f588d8b24160977a9d4688
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-07-12 16:55:37 +0300

    Add ship board.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -81,6 +81,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
 
             # Очистка списков пришельцев и снарядов.
@@ -152,6 +153,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Увелечение уровня.
+            self.stats.level += 1
+            self.sb.prep_level()
 
 
     def _update_aliens(self):
